abd6edf9c8a4ccfa1c7d72e6fef8e96bf59bc803
MaximeBrandely <35151348+MaximeBrandely@users.noreply.github.com>
2018-01-17 18:10:32 +0000

    Update Mise_en_chauffe.rb

--- Mise_en_chauffe.rb
+++ Mise_en_chauffe.rb
@@ -5,10 +5,10 @@ Binding.pry #
 
 # quelques lignes qui enregistrent les clés d'APIs
 client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = "Ehq9mVtjEXZ57vwq8TOTtxOeQ"
-  config.consumer_secret     = "x1kapuaFqqJBhvTtcDddOngDkxNokp0iRN7Z2KLI4r9f30NCc8"
-  config.access_token        = "841667595359121409-Vz3Xy43gS4Xut9l3C67wHNW2Rs65HPa"
-  config.access_token_secret = "aohuZJlemtb75ijZ0hDSjJFRrhqdZI6cJrUtRJb9FHlNZ"
+  config.consumer_key        = "key"
+  config.consumer_secret     = "key"
+  config.access_token        = "key"
+  config.access_token_secret = "key"
 end
 
 # ligne qui permet de tweeter
